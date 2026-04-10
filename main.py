61041b42e55eca21ce6758268c14dd5846724280
Blackbottt <ryanchipir@gmail.com>
2026-04-10 15:17:34 +0200

    added validator funtion and commented category function

--- main.py
+++ main.py
@@ -1,26 +1,69 @@
 import datetime
 import json
 
-def add_expense():
-    name = input("Name: ")
-    while not name.strip():
-        print("Fehler: Bitte Input Name als Wörter")
-        name = input("Name: ")
+def validator(data_type, prompt):
+    if data_type == str:
+        user_input = input(prompt)
 
+        while not user_input.strip():
+            print("Fehler: Bitte Input Name als Wörter")
+            user_input = input("Name: ")
+    
+        return user_input
+    
+    elif data_type == float:
+        while True:
+            user_input = input(prompt)
+            try:
+                user_input = float(user_input)
+                if user_input <= 0:
+                    print("Bitte positiven Betrag eingeben")
+                else:
+                    break
+            except ValueError:
+                print("Bitte positiven Betrag eingeben")
+
+        return user_input
+
+    elif data_type == int:
+        while True:
+            user_input = input(prompt)
+            try:
+                user_input = int(user_input)
+                break
+            except ValueError:
+                print("Bitte positiven Betrag eingeben")
+        
+        return user_input
+
+    else:
+        raise ValueError("Unsupported data type")
+
+# def add_category():
+#     print("Budget typisieren: \n1. Geschaft \n2. Persönlich \n3. Haushalt Nebenkosten")
+#     budget_type = input("Wählen Ihrer Budget typisieren: ")
+
+#     try:
+#         budget_type = int(budget_type)
+#     except ValueError:
+#         print("Bitte existiert Betrag eingeben")
+#     print("Budget typisieren: \n1. Geschaft \n2. Persönlich \n3. Haushalt Nebenkosten")
+#     # category = input("Category: ")
+#     if budget_type == "1":
+#         print("Kategorian: \n1. Werbetreibend\n2. Bürobedarfsartikel\n3. Reiseausgaben \n4. Versorgungswirtschaft \n5. Beratungskosten")
+
+#     elif budget_type == "2":
+#         print("Kategorian: \n1. Wohnungs (Miete/Belehnen) \n2. Verkehrsmittel (Benzin/Öffentliche) \n3. Essen (Lebensmittel/außer Haus) \n4. Gesundheit (Versicherung/ Medizinische Ausgaben) /n5. 	Unterhaltung")
+
+#     elif budget_type == "3":
+#         print("Kategorian: \n1. Nebenkosten (Elekrizitat/Wasser) \n2. Versicherung (Hause/Auto) \n3. Abspeicherungen (Vorsorgevermögen/Notfallfonds) \n4. Bildung (Unterrichtsgebühr/Materialen)")
+
+def add_expense():
+    name = validator(str, "Name :")
+    amount =  validator(float, "Betrag :")
     date_iso = datetime.datetime.now().isoformat()
     date = datetime.datetime.fromisoformat(date_iso).strftime("%a %d %B %Y")
-
-    while True:
-        amount = input("Betrag: ")
-        try:
-            amount = float(amount)
-            if amount <= 0:
-                print("Bitte positiven Betrag eingeben")
-            else:
-                break
-        except ValueError:
-            print("Bitte positiven Betrag eingeben")
-
+    # add_category()
     return {"name": name, "amount": amount, "date": date}
 
 def show_expenses(expenses):
@@ -33,7 +76,6 @@ def show_expenses(expenses):
             print(f'\n{i}. {expense["name"]} | ${expense["amount"]:.2f} | {expense["date"]}')
         else:
             print(f'{i}. {expense["name"]} | ${expense["amount"]:.2f} | {expense["date"]}')
-
 
     total = sum(expense["amount"] for expense in expenses)
     print(f"\nGesamt Budget ist {total:.2f}")
@@ -87,6 +129,7 @@ while True:
 
     if wähle == "1":
         expense = add_expense()
+        print("expense", expense)
         expenses.append(expense)
         save_expenses(expenses)
 
